a4650d2e0c2a0fba0865e1f1f314dca764136f88
Junhua Chang <junhua.chang@napster.com>
2025-08-20 15:49:32 -0700

    Fix relative path import

--- real-time-gui.py
+++ real-time-gui.py
@@ -18,17 +18,17 @@ import yaml
 warnings.simplefilter("ignore")
 
 from tqdm import tqdm
-from .modules.commons import *
+from modules.commons import *
 import librosa
 import torchaudio
 import torchaudio.compliance.kaldi as kaldi
 
-from .hf_utils import load_custom_model_from_hf
+from hf_utils import load_custom_model_from_hf
 
 import os
 import sys
 import torch
-from .modules.commons import str2bool
+from modules.commons import str2bool
 # Load model and configuration
 device = None
 
@@ -173,7 +173,7 @@ def load_models(args):
     model.cfm.estimator.setup_caches(max_batch_size=1, max_seq_length=8192)
 
     # Load additional modules
-    from .modules.campplus.DTDNN import CAMPPlus
+    from modules.campplus.DTDNN import CAMPPlus
 
     campplus_ckpt_path = load_custom_model_from_hf(
         "funasr/campplus", "campplus_cn_common.bin", config_filename=None
@@ -186,7 +186,7 @@ def load_models(args):
     vocoder_type = model_params.vocoder.type
 
     if vocoder_type == 'bigvgan':
-        from .modules.bigvgan import bigvgan
+        from modules.bigvgan import bigvgan
         bigvgan_name = model_params.vocoder.name
         bigvgan_model = bigvgan.BigVGAN.from_pretrained(bigvgan_name, use_cuda_kernel=False)
         # remove weight norm in the model and set to eval mode
@@ -194,8 +194,8 @@ def load_models(args):
         bigvgan_model = bigvgan_model.eval().to(device)
         vocoder_fn = bigvgan_model
     elif vocoder_type == 'hifigan':
-        from .modules.hifigan.generator import HiFTGenerator
-        from .modules.hifigan.f0_predictor import ConvRNNF0Predictor
+        from modules.hifigan.generator import HiFTGenerator
+        from modules.hifigan.f0_predictor import ConvRNNF0Predictor
         hift_config = yaml.safe_load(open('configs/hifigan.yml', 'r'))
         hift_gen = HiFTGenerator(**hift_config['hift'], f0_predictor=ConvRNNF0Predictor(**hift_config['f0_predictor']))
         hift_path = load_custom_model_from_hf("FunAudioLLM/CosyVoice-300M", 'hift.pt', None)
@@ -316,7 +316,7 @@ def load_models(args):
         "fmax": None if config['preprocess_params']['spect_params'].get('fmax', "None") == "None" else 8000,
         "center": False
     }
-    from .modules.audio import mel_spectrogram
+    from modules.audio import mel_spectrogram
 
     to_mel = lambda x: mel_spectrogram(x, **mel_fn_args)
 
